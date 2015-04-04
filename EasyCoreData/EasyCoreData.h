07610d2ebd89a2caebaa8558b1a3cd70d55b5381
mti <mti@LT0478-M.local>
2015-04-04 13:46:19 -0400

    Initial Commit

---
+++ EasyCoreData/EasyCoreData.h
@@ -0,0 +1,19 @@
+//
+//  EasyCoreData.h
+//  EasyCoreData
+//
+//  Created by mti on 4/4/15.
+//  Copyright (c) 2015 vlad orby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyCoreData.
+FOUNDATION_EXPORT double EasyCoreDataVersionNumber;
+
+//! Project version string for EasyCoreData.
+FOUNDATION_EXPORT const unsigned char EasyCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyCoreData/PublicHeader.h>
+
+
